5200014fed872fb4ed8bf276722fa54a53385837
Tyrel Clayton <tyrelclayton92@gmail.com>
2026-07-23 00:48:03 -0400

    swap to vite

--- app/root.tsx
+++ app/root.tsx
@@ -3,7 +3,6 @@ import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import {
   Links,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
@@ -33,7 +32,6 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
       </body>
     </html>
   );
