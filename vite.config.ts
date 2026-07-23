649467bf0c6f6e6106d2637eb960756202751256
Tyrel Clayton <tyrelclayton92@gmail.com>
2026-07-23 14:12:37 -0400

    really finish

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,6 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  // or cloudflare, deno, etc.
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
-
 export default defineConfig({
   server: {
     port: 3000,
