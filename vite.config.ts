f5eb00d412d7b360167b500d1b5d0ecde460a015
Tyrel Clayton <tyrelclayton92@gmail.com>
2026-07-24 06:10:23 -0400

    bump vite version

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [reactRouter()],
 });
